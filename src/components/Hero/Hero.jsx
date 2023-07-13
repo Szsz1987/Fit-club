6cfd2896756c84add54f9fac58b6f914f88f672b
Szymon Szuliński <sszulinski@gmail.com>
2023-07-13 07:36:15 +0200

    Finish the Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Hero.css';
 import Header from '../Header/Header';
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import heart from "../../assets/heart.png";
+import calories from "../../assets/calories.png";
 
 const Hero = () => {
     return (
@@ -50,9 +54,29 @@ const Hero = () => {
                     <button className="btn">Learn More</button>
                 </div>
             </div>    
-            <div className="right-h">right side</div>
+            <div className="right-h">
+                <button className="btn">Join now</button>
+                <div className="heart-rate">
+                    <img src={heart} alt=""/>
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+                {/* Hero images*/}
+                <img src={hero_image} alt="" className="hero-image" />
+                <img src={hero_image_back} alt="" className="hero-image-back" />
+
+                {/* Calories */}
+                <div className="calories">
+                    <img src={calories} alt=""/>
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>    
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Hero
