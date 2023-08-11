1d515914f1dbe86395b59d9be6c10fe17c3cc360
Szymon Szuliński <sszulinski@gmail.com>
2023-08-11 07:40:35 +0200

    Start work on a new component (Join)

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Join.css';
+
+const Join = () => {
+    return(
+        <div className="Join" id="join-us">
+            <div className="left-j">
+                <div>
+                    <span>ready to</span>
+                    <span>level up</span>
+                </div> 
+                <div>
+                    <span>your body</span>
+                    <span>with us</span>
+                </div>
+            </div>
+            <div className="right-j"></div>
+        </div>
+    )
+}
+
+export default Join;
