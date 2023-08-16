fd543032b21e310d977ca06d2209c6d156224f26
Szymon Szuliński <sszulinski@gmail.com>
2023-08-16 19:17:31 +0200

    Add EmailJs to the form of the Join component

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Join.css';
+import emailjs from '@emailjs/browser';
 
 const Join = () => {
+        const form = useRef()
+
+        const sendEmail = (e) => {
+            e.preventDefault();
+        
+            emailjs.sendForm('service_44aqv7c', 'template_i0f5g4s', form.current, 'ePkjOn8VzO6O39u63')
+              .then((result) => {
+                  console.log(result.text);
+              }, (error) => {
+                  console.log(error.text);
+              });
+          };
+
     return(
         <div className="Join" id="join-us">
             <div className="left-j">
@@ -16,7 +30,7 @@ const Join = () => {
                 </div>
             </div>
             <div className="right-j">
-                <form action="" className="email-container">
+                <form ref={form} className="email-container" onSubmit={sendEmail}>
                     <input type="email" name="user_email"
                     placeholder="Enter your Email address"/>
                     <button className="btn btn-j">Join now</button>
